96304f9084f1ea0a0bbf0c59b8cd7aacefb41cb5
Roman Alenskiy <kuperman88@gmail.com>
2018-07-08 21:57:17 +0300

    Added Categories model and controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
   def about
+    @categories = Category.all
   end
 
   def history
+    @categories = Category.all
   end
 
   def contacts
+    @categories = Category.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get '/contacts',    to: 'static_pages#contacts'
   get '/about',   to: 'static_pages#about'
   get '/history', to: 'static_pages#history'
+  #Categories
+  resources :categories
+  #Products
+  resources :products
 end
