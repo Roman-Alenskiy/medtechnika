6f6c7effd4e930bfbdb8f21397261385a5bfd505
Roman Alenskiy <kuperman88@gmail.com>
2018-07-11 23:40:49 +0300

    Bug fixed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,5 @@
 class CategoriesController < ApplicationController
     def show 
-        @categories_first_h = Category.first(Category.count / 2 + 1)
-        @categories_second_h = Category.offset(Category.count / 2 + 1)
         @category = Category.find(params[:id])
         @products = @category.products
     end   
