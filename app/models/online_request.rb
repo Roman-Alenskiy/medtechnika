144a5adbeca375b44f502e3754b7f6735cfe1103
Roman Alenskiy <kuperman88@gmail.com>
2018-08-29 18:25:46 +0300

    Added phone number validation

--- app/models/online_request.rb
+++ app/models/online_request.rb
@@ -5,10 +5,13 @@ class OnlineRequest < ApplicationRecord
     before_save :downcase_email
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    VALID_PHONE_REGEX = /\A[\d\s\-\+]+\z/
 
     validates :customer_name, presence: true, length: {maximum: 30} 
     validates :customer_email, presence: true, length: {maximum: 255}, 
               format: {with: VALID_EMAIL_REGEX}
+    validates :customer_phone_number, length: {minimum:3, maximum: 30},
+              format: {with: VALID_PHONE_REGEX}
     validates :subject, presence: true, length: {maximum: 255}
     validates :message, presence: true
 
